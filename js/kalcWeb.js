26ee01809305fd106c13ef4941693ba4c26ca8ff
fontineles <fontineleslucas@gmail.com>
2021-10-26 14:33:41 -0300

    Desenvolvimento de uma calculadora web

---
+++ js/kalcWeb.js
@@ -0,0 +1,77 @@
+console.log('Teste Calculadora');
+
+// DOM
+
+// Operações
+const igual = document.getElementById("igual");
+const soma = document.getElementById("soma")
+const subtracao = document.getElementById("subtracao")
+const divisao = document.getElementById("divisao")
+const multiplicacao = document.getElementById("multiplicacao")
+
+// Números
+const nove = document.getElementById("nove")
+const oito = document.getElementById("oito")
+const sete = document.getElementById("sete")
+const seis = document.getElementById("seis")
+const cinco = document.getElementById("cinco")
+const quatro = document.getElementById("quatro")
+const tres = document.getElementById("tres")
+const dois = document.getElementById("dois")
+const um = document.getElementById("um")
+const limpar = document.getElementById("clear")
+const ponto = document.getElementById(".")
+const zero = document.getElementById("0")
+
+//Eventos
+igual.onclick = function() {
+    console.log(igual.innerText)
+}
+soma.onclick = function() {
+    console.log(soma.innerText)
+}
+subtracao.onclick = function() {
+    console.log(subtracao.innerText)
+}
+divisao.onclick = function() {
+    console.log(divisao.innerText)
+}
+multiplicacao.onclick = function() {
+    console.log(multiplicacao.innerText)
+}
+nove.onclick = function() {
+    console.log(nove.innerText)
+}
+oito.onclick = function() {
+    console.log(oito.innerText)
+}
+sete.onclick = function() {
+    console.log(sete.innerText)
+}
+seis.onclick = function() {
+    console.log(seis.innerText)
+}
+cinco.onclick = function() {
+    console.log(cinco.innerText)
+}
+quatro.onclick = function() {
+    console.log(quatro.innerText)
+}
+tres.onclick = function() {
+    console.log(tres.innerText)
+}
+dois.onclick = function() {
+    console.log(dois.innerText)
+}
+um.onclick = function() {
+    console.log(um.innerText)
+}
+limpar.onclick = function() {
+    console.log(limpar.innerText)
+}
+ponto.onclick = function() {
+    console.log(ponto.innerText)
+}
+zero.onclick = function() {
+    console.log(zero.innerText)
+}
